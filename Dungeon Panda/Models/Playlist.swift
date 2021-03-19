7876d86ee7ab257dd105fd56c08d7f7df36302bf
Andrew Hodgkinson <ahodgkin@rowing.org.uk>
2021-03-19 14:04:03 +1300

    Restructure internally, sort out UI

---
+++ Dungeon Panda/Models/Playlist.swift
@@ -0,0 +1,13 @@
+//
+//  Playlist.swift
+//  Dungeon Panda
+//
+//  Created by Andrew Hodgkinson on 18/03/21.
+//
+
+import Foundation
+
+struct Playlist {
+    var name: String
+    var tracks: [Track]
+}
---
+++ Dungeon Panda/Models/Track.swift
@@ -0,0 +1,17 @@
+//
+//  Track.swift
+//  Dungeon Panda
+//
+//  Created by Andrew Hodgkinson on 18/03/21.
+//
+
+import Foundation
+
+struct Track {
+    var song_id: String
+    var start_seconds: UInt16
+    var end_seconds: UInt16
+    var fade_in: Bool
+    var fade_out: Bool
+}
+
